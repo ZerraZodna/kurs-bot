391a4ffef9e6c64fdbc1c669ef1a38e3b52a776b
Steen <john3.6steen@gmail.com>
2026-03-29 00:02:39 +0100

    Add icons to help message

---
+++ swarm/swarm/help.py
@@ -0,0 +1,11 @@
+from langchain_core.prompts import PromptTemplate
+
+HELP_MESSAGE = """
+📋 Available Commands:
+  • /start - Initialize the bot
+  • /help - Show help message
+  • /clear - Clear current session
+  • /reset - Reset all data
+
+  📞 For support: contact@example.com
+"""
